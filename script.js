681d1ef85b3b6cbdc330c7e13c957238b4bfae5f
Gero <gersonhoa@gmail.com>
2021-12-07 21:26:49 -0300

    100%

--- script.js
+++ script.js
@@ -22,7 +22,6 @@ btnLogin.addEventListener('click', itsTryber);
 
 // requisito de travar o botão sem checkbox:
 
-
 buttonEnviar.disabled = true;
 const checkBox = document.getElementById('agreement');
 
@@ -52,12 +51,51 @@ textArea.addEventListener('keyup', contador);
 
 // requisito recuperar o texto
 
+const getStudantName = () => {
+  const name = document.getElementById('input-name');
+  const surname = document.getElementById('input-lastname');
+  const elementParent = name.parentNode;
+  const newElement = document.createElement('p');
+  newElement.innerText = `Nome: ${name.value} ${surname.value} `;
+  elementParent.appendChild(newElement);
+  elementParent.removeChild(name);
+  elementParent.removeChild(surname);
+};
+
+const getData = (id, apoioTxt) => {
+  const data = document.querySelector(id);
+  const dataParent = data.parentNode;
+  const newElement = document.createElement('p');
+  newElement.innerText = `${apoioTxt}: ${data.value}`;
+  dataParent.appendChild(newElement);
+  console.log(newElement.innerText);
+  dataParent.removeChild(data);
+};
+
+const getSubjects = () => {
+  const data = document.querySelectorAll('.subject:checked');
+  console.log(data);
+  const dataParent = data[0].parentNode;
+  const newElement = document.createElement('p');
+  newElement.innerText = 'Matérias:';
+  for (let i = 0; i < data.length; i += 1) {
+    newElement.innerText += ` ${data[i].defaultValue}`;
+    if (i + 1 !== data.length)newElement.innerText += ',';
+  }
+  console.log(newElement.innerText);
+  dataParent.appendChild(newElement);
+  
+};
+
 function changeText(event) {
   event.preventDefault();
-  let commander = '';
-  commander = document.createElement('p')
-  document.getElementsByTagName('body')[0].appendChild(commander)
-  commander.innerText = document.getElementById('input-name').value
+  getStudantName();
+  getData('#input-email', 'Email');
+  getData('#house', 'Casa');
+  getData('input[name="family"]:checked', 'Família');
+  getData('input[name="rate"]:checked', 'Avaliação');
+  getData('#textarea', 'Observações');
+  getSubjects();
 }
-
-buttonEnviar.addEventListener('click',changeText )
+// teste pra commit
+buttonEnviar.addEventListener('click', changeText);
